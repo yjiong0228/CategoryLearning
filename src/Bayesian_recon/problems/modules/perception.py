dad16d677f821398a1f3077a2af0203293baa6ea
Xie Hanliang <xiehanliang0917@qq.com>
2025-05-11 11:32:51 +0000

    fix(optimizer, perception): optimize_params_with_subs_parallel, sample

--- src/Bayesian_recon/problems/modules/perception.py
+++ src/Bayesian_recon/problems/modules/perception.py
@@ -2,14 +2,14 @@
 Module: Perception Mechanism
 """
 
-from typing import Dict
+from typing import Dict, List
 import numpy as np
 import os
 import pandas as pd
 from .base_module import BaseModule
 
-DEFAULT_PROCESSED_DATA_PATH = os.path.join(
-    os.path.dirname(__file__), "../../../../data/processed/Task1b_processed.csv")
+DEFAULT_PROCESSED_DATA_DIR = os.path.join(
+    os.path.dirname(__file__), "../../../../data/processed")
 
 class BasePerception(BaseModule):
     """
@@ -25,11 +25,12 @@ class BasePerception(BaseModule):
             **kwargs: Additional keyword arguments
         """
         super().__init__(model, **kwargs)
-        processed_data_path = kwargs.pop("processed_data_path", DEFAULT_PROCESSED_DATA_PATH)
+        self.processed_data_dir = kwargs.pop("processed_data_path", DEFAULT_PROCESSED_DATA_DIR)
         self.variances : Dict[str, Dict[str, float]] = {}
-        processed_data = pd.read_csv(processed_data_path)
+        processed_data = pd.read_csv(os.path.join(self.processed_data_dir, "Task1b_processed.csv"))
         error = self.error_calculation(processed_data)
         self.variances = self.variances_calculation(error)
+        self.structures : Dict[str, List]= self.get_structures()
 
     def error_calculation(self, processed_data):
         """
@@ -71,7 +72,24 @@ class BasePerception(BaseModule):
         variances = variances.to_dict(orient='index')
         variances = {k: {key.split('_length')[0]: value for key, value in v.items()} for k, v in variances.items()}
         return variances
-    
+
+    def get_structures(self):
+        """
+        Get structures for each subject
+
+        Returns:
+            dict: Structures for each subject
+        """
+        structures = {}
+        processed_data = pd.read_csv(os.path.join(self.processed_data_dir, "Task2_processed.csv"))
+
+        for iSub, group in processed_data.groupby('iSub'):
+            structures[iSub] = group[['structure1', 'structure2']].values
+            assert np.all(structures[iSub] == structures[iSub][0]), f"iSub {iSub} has inconsistent structures."
+            structures[iSub] = structures[iSub][0].tolist()
+
+        return structures
+
     def sample(self, iSub, stimulus):
         """
         Sample from the model
@@ -84,8 +102,39 @@ class BasePerception(BaseModule):
         """
         if iSub not in self.variances:
             raise ValueError(f"iSub {iSub} not found in variances.")
+        
+        def convert(structure):
+            # feature selection
+            if structure[0] == 1:
+                features = ["neck", "head", "leg", "tail"]
+            elif structure[0] == 2:
+                features = ["neck", "head", "tail", "leg"]
+            elif structure[0] == 3:
+                features = ["neck", "leg", "tail", "head"]
+            elif structure[0] == 4:
+                features = ["head", "leg", "tail", "neck"]
 
-        feat = ['leg', 'head', 'tail', 'neck']
+            # feature space segmentation
+            if structure[1] == 1:
+                features = features[:]
+            elif structure[1] == 2:
+                features = [features[0], features[2], features[1], features[3]]
+            elif structure[1] == 3:
+                features = [features[1], features[0], features[2], features[3]]
+            elif structure[1] == 4:
+                features = [features[1], features[2], features[0], features[3]]
+            elif structure[1] == 5:
+                features = [features[2], features[0], features[1], features[3]]
+            elif structure[1] == 6:
+                features = [features[2], features[1], features[0], features[3]]
+
+            # Final rearrangement
+            features = [features[0], features[2], features[1], features[3]]
+
+            # Add suffix to feature names
+            return features
+        
+        feat = convert(self.structures[iSub])
         n_trials = len(stimulus)
         for i in range(stimulus.shape[1]):
             stimulus[:, i] = stimulus[:, i] + np.random.normal(
